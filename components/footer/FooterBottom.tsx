43dd10ffbd495d253ee79cb566d7fe15240d4ba9
herbsdokta79 <90586173+herbsdokta79@users.noreply.github.com>
2024-12-05 21:14:23 +0000

    Update FooterBottom.tsx

--- components/footer/FooterBottom.tsx
+++ components/footer/FooterBottom.tsx
@@ -7,7 +7,7 @@ export function FooterBottom() {
   return (
     <div className={styles.footerBottom}>
       <p className={styles.copyright}>
-        © {currentYear} Modern Clinic. All rights reserved.
+        © {currentYear} LUXE CLINIC. All rights reserved.
       </p>
       <p className={styles.attribution}>
         Designed by <Link href="https://tricky-tau.vercel.app/" className="underline text-blue-500">Patrick Annang</Link>
