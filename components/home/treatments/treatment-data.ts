74965a1e7d3530d42fee1f29919c25df9fb19916
herbsdokta79 <90586173+herbsdokta79@users.noreply.github.com>
2024-12-25 14:41:51 +0000

    Update treatment-data.ts

--- components/home/treatments/treatment-data.ts
+++ components/home/treatments/treatment-data.ts
@@ -7,19 +7,19 @@ import {
 export const treatments = [
   {
     id: "aesthetic",
-    title: "Medical Aesthetics",
+    title: "Medical Dermatology",
     description: "These treatments focus on enhancing physical appearance by improving skin texture, tone, and overall beauty.",
     icon: Sparkles,
   },
   {
     id: "medical",
-    title: "Medical Treatments",
+    title: "Medical Aesthetics",
     description: "These therapies address specific health or medical conditions, including skin regeneration and hormone management.",
     icon: Syringe,
   },
   {
     id: "wellness",
-    title: "Wellness Programs",
+    title: "Family Planning & Wellness",
     description: "These treatments aim to improve overall health, hydration, and vitality, contributing to a balanced lifestyle.",
     icon: UserPlus,
   }
