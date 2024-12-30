3c49dd6a32864d685eabfd78952374e5a5be4630
Patrick Amartey Annang <102452248+amartey-tricky@users.noreply.github.com>
2024-12-30 19:51:31 +0000

    Update treatment-data.ts

--- components/home/treatments/treatment-data.ts
+++ components/home/treatments/treatment-data.ts
@@ -7,13 +7,13 @@ import {
 export const treatments = [
   {
     id: "aesthetic",
-    title: "Dermatology",
+    title: "Medical Aesthetics",
     description: "Acne treatment, Psoriasis management, Eczema treatment, Skin cancer screening and treatment, Rosacea treatment, Hyperpigmentation treatment, Hair loss treatment, Nail fungus treatment, Wart removal, Skin allergy testing and treatment.",
     icon: Sparkles,
   },
   {
     id: "medical",
-    title: "Medical Aesthetics",
+    title: "Medical Treatments",
     description: " Botox and filler injections, Chemical peels, Microdermabrasion, Laser hair removal, Laser skin rejuvenation, Microneedling, Platelet-rich plasma (PRP) therapy, Facial contouring, Skin tightening treatments",
     icon: Syringe,
   },
