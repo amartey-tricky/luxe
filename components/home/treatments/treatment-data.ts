ab9fd51fe4730fd6d31804b3bd9923161f8159e2
herbsdokta79 <90586173+herbsdokta79@users.noreply.github.com>
2025-01-31 19:12:47 +0000

    Update treatment-data.ts

--- components/home/treatments/treatment-data.ts
+++ components/home/treatments/treatment-data.ts
@@ -6,21 +6,21 @@ import {
 
 export const treatments = [
   {
+    id: "wellness",
+    title: "Dermatology",
+    description: "At Luxe, our board-certified dermatologists provide comprehensive care for all skin types and concerns. From routine check-ups and skin exams to advanced treatments for complex conditions, we're dedicated to delivering exceptional dermatological care. Our state-of-the-art facilities and cutting-edge technologies ensure accurate diagnoses and effective treatments, helping you achieve the healthy, radiant skin you deserve.",
+    icon: UserPlus,
+   },
+  {
     id: "aesthetic",
     title: "Medical Aesthetics",
-    description: "Acne treatment, Psoriasis management, Eczema treatment, Skin cancer screening and treatment, Rosacea treatment, Hyperpigmentation treatment, Hair loss treatment, Nail fungus treatment, Wart removal, Skin allergy testing and treatment.",
+    description: "Experience the transformative power of medical aesthetics at Luxe. Our expert team offers personalized treatments tailored to address a range of skin concerns, from fine lines and wrinkles to hyperpigmentation and scarring. Using the latest advancements in cosmetic technology, we'll work with you to create a customized plan that leaves you looking and feeling your absolute best.",
     icon: Sparkles,
   },
   {
     id: "medical",
     title: "Medical Treatments",
-    description: " Botox and filler injections, Chemical peels, Microdermabrasion, Laser hair removal, Laser skin rejuvenation, Microneedling, Platelet-rich plasma (PRP) therapy, Facial contouring, Skin tightening treatments",
+    description: "At Luxe, our medical therapy services focus on delivering holistic, patient-centered care. Our experienced healthcare professionals work closely with you to develop personalized treatment plans, addressing a wide range of medical concerns, including chronic diseases, infections, and more. With a commitment to compassionate, evidence-based care, we prioritize your health, comfort, and overall well-being.",
     icon: Syringe,
-  },
-  {
-    id: "wellness",
-    title: "Family Planning & Wellness",
-    description: "These treatments aim to improve overall health, hydration, and vitality, contributing to a balanced lifestyle.",
-    icon: UserPlus,
   }
 ];
