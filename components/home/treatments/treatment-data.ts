b5d0f9fc1d1a85e959d8d9fe44a9126b39a20440
herbsdokta79 <90586173+herbsdokta79@users.noreply.github.com>
2025-02-04 08:55:55 +0000

    Update treatment-data.ts

--- components/home/treatments/treatment-data.ts
+++ components/home/treatments/treatment-data.ts
@@ -29,5 +29,5 @@ export const treatments = [
     title: "Dental Therapy",
     description: "At Luxe Clinic, our team of expert dentists and hygienists provide comprehensive general dental services, utilizing advanced technology and techniques to deliver personalized care with precision and compassion. From routine check-ups and cleanings to fillings, extractions, oral surgery, and emergency dental care, we are dedicated to ensuring optimal oral health and a beautiful smile for each and every patient. Our goal is to provide a comfortable, relaxing, and stress-free experience, while educating and guiding you towards optimal oral health and wellness.",
     icon: Leaf,
-  },
+  }
 ];
