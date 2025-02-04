353a2ed0206412835e811cb4175e5d1bcf0baac7
tricky <trickya23@outlook.com>
2025-02-04 08:15:12 +0000

    add keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const description = "A clinic management system for Luxe Clinic"
 export const metadata: Metadata = {
   title: title,
   description: description,
-  keywords: "luxe clinic, aesthetics, botox, fillers, clinic, Tamale, tamale, Ang, Patty, dermatology, skincare, skin, acne, facials",
+  keywords: "luxe clinic, aesthetics, botox, fillers, clinic, Tamale, tamale, Ang, Patty, dermatology, skincare, skin, acne, facials, luxe clinic, luxe clinic gh, spas in tamale, best clinic in tamale, beauty and wellness clinic, Aesthetic beauty, aesthetic medicine, threads, Skin care, stretch marks removal, acne, skin tags, DPN, cystic acne, hormonal acne, acne scars, hyper pigmentation, scar treatment, skin diseases, Gonorrhea, family planning, UTIs, medical dermatology, cosmetic dermatology, pediatric dermatology, gynecology, candidiasis, bacterial vaginosis, thrush, vaginal discharge, anti-aging treatment, wrinkling and fine line treatment, skin clinic in tamale, clinic in tamale, pimples treatment, spa, glass skin, skin care, medical grade cosmetic product, laser hair removal, tattoo removal, laser treatments, laser scar treatment, luxe clinic tamale, luxe clinic gh, medical facials, teeth whitening, dental scaling,teeth polishing, skin brightening IV, detox drip, weight loss drip, weight loss, hangover drip, IV Cocktail, myar’s cocktail, IV nutrition, IV therapy, IV drips, skincare routine, beauty, rejuvenation, hep B, hep C, hypertension, diabetes, psoriasis, eczema, vaginal tightening, P.O shots, fillers, non surgical BBL, IV BBL",
   openGraph: {
     title: title,
     description: description,
