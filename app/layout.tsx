61400e3b2bdb0514ba4cc87923ead78b2eb77a02
herbsdokta79 <90586173+herbsdokta79@users.noreply.github.com>
2025-02-09 15:55:53 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,13 +17,13 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-const title = "Luxe Clinic";
-const description = "A clinic management system for Luxe Clinic"
+const title = "Luxe Clinic, Tamale";
+const description = "At Luxe Clinic, we combine innovative medical care with exceptional luxury amenities to create a truly unique healthcare experience. Our compassionate professionals provide personalized attention, expert guidance, and comprehensive services to address your every health concern"
 
 export const metadata: Metadata = {
   title: title,
   description: description,
-  keywords: "luxe clinic, aesthetics, botox, fillers, clinic, Tamale, tamale, Ang, Patty, dermatology, skincare, skin, acne, facials, luxe clinic, luxe clinic gh, spas in tamale, best clinic in tamale, beauty and wellness clinic, Aesthetic beauty, aesthetic medicine, threads, Skin care, stretch marks removal, acne, skin tags, DPN, cystic acne, hormonal acne, acne scars, hyper pigmentation, scar treatment, skin diseases, Gonorrhea, family planning, UTIs, medical dermatology, cosmetic dermatology, pediatric dermatology, gynecology, candidiasis, bacterial vaginosis, thrush, vaginal discharge, anti-aging treatment, wrinkling and fine line treatment, skin clinic in tamale, clinic in tamale, pimples treatment, spa, glass skin, skin care, medical grade cosmetic product, laser hair removal, tattoo removal, laser treatments, laser scar treatment, luxe clinic tamale, luxe clinic gh, medical facials, teeth whitening, dental scaling,teeth polishing, skin brightening IV, detox drip, weight loss drip, weight loss, hangover drip, IV Cocktail, myar’s cocktail, IV nutrition, IV therapy, IV drips, skincare routine, beauty, rejuvenation, hep B, hep C, hypertension, diabetes, psoriasis, eczema, vaginal tightening, P.O shots, fillers, non surgical BBL, IV BBL",
+  keywords: "luxe clinic,skin clinic in tamale, dermatology clinic in tamale, luxe clinic tamale, aesthetics, botox, fillers, clinic, Tamale, tamale, Ang, Patty, dermatology, skincare, skin, acne, facials, luxe clinic, luxe clinic gh, spas in tamale, best clinic in tamale, beauty and wellness clinic, Aesthetic beauty, aesthetic medicine, threads, Skin care, stretch marks removal, acne, skin tags, DPN, cystic acne, hormonal acne, acne scars, hyper pigmentation, scar treatment, skin diseases, Gonorrhea, family planning, UTIs, medical dermatology, cosmetic dermatology, pediatric dermatology, gynecology, candidiasis, bacterial vaginosis, thrush, vaginal discharge, anti-aging treatment, wrinkling and fine line treatment, skin clinic in tamale, clinic in tamale, pimples treatment, spa, glass skin, skin care, medical grade cosmetic product, laser hair removal, tattoo removal, laser treatments, laser scar treatment, luxe clinic tamale, luxe clinic gh, medical facials, teeth whitening, dental scaling,teeth polishing, skin brightening IV, detox drip, weight loss drip, weight loss, hangover drip, IV Cocktail, myar’s cocktail, IV nutrition, IV therapy, IV drips, skincare routine, beauty, rejuvenation, hep B, hep C, hypertension, diabetes, psoriasis, eczema, vaginal tightening, P.O shots, fillers, non surgical BBL, IV BBL",
   openGraph: {
     title: title,
     description: description,
